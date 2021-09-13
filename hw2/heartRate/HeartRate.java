cff1cc72f97dc500bd2561759cad76841b908571
Gary Montgomery <g.s.montgomery@vikes.csuohio.edu>
2021-09-13 08:49:55 -0400

    finish heart rate assignment section

--- hw2/heartRate/HeartRate.java
+++ hw2/heartRate/HeartRate.java
@@ -1,2 +1,37 @@
-public static void main(String[] args) throws IOException {
+import java.io.File;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class HeartRate {
+    public static void main(String[] args) throws IOException {
+        File hrFile = new File("heartRate/HR.txt");
+        Scanner reader = new Scanner(hrFile);
+        int goodDataCount = 0;
+        int badDataCount = 0;
+        while (reader.hasNext()) {
+            try {
+                int data = reader.nextInt();
+                if(data < 0) {
+                    throw new HRIllegalValueException("");
+                } else{
+                    goodDataCount++;
+                }
+            } catch (InputMismatchException e) {
+                badDataCount++;
+                reader.next();
+            } catch (HRIllegalValueException e){
+                System.out.println("HRIllegalValueException: cannot be negative");
+                badDataCount++;
+            }
+        }
+        System.out.println("Good Data " + goodDataCount);
+        System.out.println("Bad Data " + badDataCount);
+    }
+}
+
+class HRIllegalValueException extends Exception{
+    HRIllegalValueException(String message){
+        super(message);
+    }
 }
